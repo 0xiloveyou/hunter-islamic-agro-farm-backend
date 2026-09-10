5d83ff05a24712d46f106f11317afed201892cfd
0xiloveyou <278306338+0xiloveyou@users.noreply.github.com>
2026-09-10 16:22:04 +0600

    feat: verifyUserEmail route

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -13,6 +13,11 @@ router.post(
 	AuthController.registerUser,
 );
 router.post(
+	"/verify-email",
+	validateRequest(UserValidation.PatientEmailVerifyZodSchema),
+	AuthController.verifyPatientEmail,
+);
+router.post(
 	"/login",
 	validateRequest(UserValidation.LoginZodSchema),
 	AuthController.loginUser,
