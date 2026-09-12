8cf12c6017a7ef5109447d6c13fb1c5a01ff03fb
0xiloveyou <278306338+0xiloveyou@users.noreply.github.com>
2026-09-12 11:10:16 +0600

    feat: cron triger on server Start

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import config from "./app/config";
+import { sendSharkApplicationSummary } from "./app/lib/cron";
 import { transporter } from "./app/lib/nodemailer";
 import { prisma } from "./app/lib/prisma";
 import { redisClient } from "./app/lib/redis";
@@ -22,6 +23,8 @@ const main = async () => {
 
 		await transporter.verify();
 		console.log("Nodemailer Connected Successfully.");
+
+        sendSharkApplicationSummary();
 
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
